f02d032454d31318a5312c81dea9ccc6748802f8
Keshav Varma <keshavdv@gmail.com>
2022-01-27 09:58:11 -0800

    Update requirements

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(name='hikvisionapi',
       long_description=open(join(dirname(__file__), 'README.md')).read(),
       download_url='https://github.com/MissiaL/hikvision-client/tarball/{}'.format(version),
       keywords=['api', 'hikvision', 'hikvision-client'],
-      install_requires=['xmltodict', 'requests']
+      install_requires=['xmltodict', 'requests', 'httpx']
       )
